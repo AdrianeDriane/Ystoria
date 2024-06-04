91c806dd2a763032684cd20e1bb89430fc941495
AdrianeDriane <dilaoaparre@gmail.com>
2024-06-04 10:18:41 +0800

    test: test without .env

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,5 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT;
-export const MONGODB_URL = process.env.MONGODB_URL;
+// export const PORT = process.env.PORT;
+export const PORT = '8080';
+// export const MONGODB_URL = process.env.MONGODB_URL;
+export const MONGODB_URL =
+  'mongodb+srv://ystoria:ystoriamongodb123@ystoria-test-db.htz4kbc.mongodb.net/?retryWrites=true&w=majority&appName=Ystoria-Test-DB';
